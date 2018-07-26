0d041b274e13efc83bc345b793974396628851bc
nxg165630 <39962685+nxg165630@users.noreply.github.com>
2018-07-26 12:33:24 -0500

    Update TestVar.java

--- Iteration 3/src/tests/TestVar.java
+++ Iteration 3/src/tests/TestVar.java
@@ -2,7 +2,7 @@ package tests;
 
 public class TestVar {
 	public static String getIndexPage() {
-		return "http://localhost:8080/Semester_Project/index.jsp";
+		return "http://localhost:8080/Iteration_3/index.jsp";
 	}
 
 }
